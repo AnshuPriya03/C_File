b4596a73b163a3dd1dcaaa5d571998a6f667919f
AnshuPriya03 <152187107+AnshuPriya03@users.noreply.github.com>
2024-08-08 18:09:28 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the value of A\n");
+    scanf("%d",&a);
+    printf("Enter the value of B\n");
+    scanf("%d",&b);
+    printf("The difference of A & B is:%d\n",a-b);
+    return 0;
+}
