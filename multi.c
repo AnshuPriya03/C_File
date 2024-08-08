a0145fd40d7bc0ddd48fa08e774de55cc49b211b
AnshuPriya03 <152187107+AnshuPriya03@users.noreply.github.com>
2024-08-08 18:08:00 +0530

    Add files via upload

---
+++ multi.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,product;
+    printf("Enter the value of a and b\n");
+    scanf("%d%d",&a,&b);
+    product=a*b;
+    printf("The product of a and b is:%d\n",product);
+    return 0;
+}
